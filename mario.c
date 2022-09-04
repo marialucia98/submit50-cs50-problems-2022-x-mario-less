8282e0fa60229ee742a0e5e95760fd5779f0efab
marialucia98 <111815667+marialucia98@users.noreply.github.com>
2022-09-04 17:31:49 -0400

    Create mario.c

---
+++ mario.c
@@ -0,0 +1,22 @@
+int main (void)
+{
+    int height,column,row
+    do
+    
+    {
+        heigth = get_int("Enter heigth here");
+    }
+
+    while(height < 1||height > 8);
+
+   {for(row =0,row < height,row++)}
+
+     printf(" ")
+
+   {for(column = 0,column <= height,column++}
+    
+    {printf("/n")}
+       
+    
+    
+    
